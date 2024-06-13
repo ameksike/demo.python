96b85f19a58a2c095d2f1dd8918ba88c0efba909
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-06-13 22:10:34 +0200

    get the environment vars

--- flask-mongodb/src/app.py
+++ flask-mongodb/src/app.py
@@ -1,5 +1,13 @@
+import os
 from flask import Flask
 from controllers.animal import animal_bp
+
+"""
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+"""
 
 def create_app():
     app = Flask(__name__)
--- flask-mongodb/src/services/db.py
+++ flask-mongodb/src/services/db.py
@@ -1,14 +1,16 @@
 from pymongo import MongoClient
+import os
 
-def getDb(name="animal", database="storage"):
+def getDb(name="animal", database="storage_"):
     client = MongoClient(
-        host='host_mongodb',
-        port=27017, 
-        username='root', 
-        password='pass',
-        authSource="admin"
+        host=os.getenv('MONGO_INITDB_HOST'),
+        port=os.getenv('MONGO_INITDB_PORT') or 27017, 
+        username=os.getenv('MONGO_INITDB_ROOT_USERNAME') or 'root_', 
+        password=os.getenv('MONGO_INITDB_ROOT_PASSWORD') or 'pass_',
+        authSource=os.getenv('MONGO_INITDB_ADM') or "admin"
     )
-    return client[database]
+    name = os.getenv('MONGO_INITDB_DATABASE') or database
+    return client[name]
     
 def delDb(conn):
     if type(conn)==MongoClient:
