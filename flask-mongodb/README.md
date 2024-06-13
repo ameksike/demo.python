9e11710832ba833e43ff10e3de3bba6cc0266eaa
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-06-13 20:26:26 +0200

    update the api

--- flask-mongodb/README.md
+++ flask-mongodb/README.md
@@ -1,4 +1,18 @@
 
-### Command 
+### Run 
 - docker-compose up
 - mongodb://root:pass@localhost:27017/admin
+- http://localhost:5000/animals
+
+### Service MongoDB
+- docker-compose up -d mongodb
+- docker-compose stop mongodb
+
+### Service Redis
+- docker-compose up -d redis
+- docker-compose stop redis
+
+### Service API
+- docker-compose up -d app
+- docker-compose stop app
+- docker logs flask-mongodb-app-1 -f
--- flask-mongodb/src/app.py
+++ flask-mongodb/src/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
-import pymongo
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
 
 app = Flask(__name__)
 
@@ -18,25 +19,88 @@ def getDb(name="animal", database="storage"):
 def ping_server():
     return "Welcome to Flask API."
 
-@app.route('/animals')
-def get_stored_animals():
+@app.route('/api/animals')
+def list_animals():
     db=""
     try:
-        db = getDb()
-        _animals = db.animal.find()
-
-        animals = [{
+        collection = getDb().animal
+        res = collection.find()
+        data = [{
             "id": animal["id"], 
             "name": animal["name"], 
             "type": animal["type"]
-        } for animal in _animals]
+        } for animal in res]
 
-        return jsonify({"data": animals})
+        return jsonify({
+            "size": len(data),
+            "data": data
+        })
     except:
         pass
     finally:
         if type(db)==MongoClient:
             db.close()
+
+@app.route('/api/animals/<int:id>', methods=['GET'])
+def select_animal(id):
+    try:
+        # _id = ObjectId(id) 
+        collection = getDb().animal
+        item  = collection.find_one({'id': int(id)})
+        if item:
+            return jsonify({
+                "id": item["id"], 
+                "name": str(item["name"]), 
+                "type": item["type"]
+            }), 200
+        else:
+            return jsonify({'error': 'Item not found'}), 404
+    except InvalidId as e:
+        return jsonify({'error': 'Invalid ID format', 'message': str(e)}), 400
+    except Exception as e:
+        return jsonify({'error': 'An error occurred', 'message': str(e)}), 500
+
+@app.route('/api/animals', methods=['POST'])
+def insert_animal():
+    try:
+        # _id = ObjectId(id) 
+        item = request.json
+        collection = getDb().animal
+        result = collection.insert_one(item)
+        return jsonify({'message': 'Item inserted', 'id': str(result.inserted_id)}), 201
+    except Exception as e:
+        return jsonify({'error': 'An error occurred', 'message': str(e)}), 500
+
+@app.route('/api/animals/<id>', methods=['PUT'])
+def update_item(id):
+    try:
+        # _id = ObjectId(id) 
+        collection = getDb().animal
+        item = request.json
+        result = collection.update_one({'id': int(id)}, {'$set': item})
+        if result.modified_count == 1:
+            return jsonify({'message': 'Item updated'}), 200
+        else:
+            return jsonify({'error': 'Item not found or not modified'}), 404
+    except InvalidId:
+        return jsonify({'error': 'Invalid ID format'}), 400
+    except Exception as e:
+        return jsonify({'error': 'An error occurred', 'message': str(e)}), 500
+
+@app.route('/api/animals/<id>', methods=['DELETE'])
+def delete_item(id):
+    try:
+        # item_id = ObjectId(id)
+        collection = getDb().animal
+        result = collection.delete_one({'id': int(id)})
+        if result.deleted_count == 1:
+            return jsonify({'message': 'Item deleted'}), 200
+        else:
+            return jsonify({'error': 'Item not found'}), 404
+    except InvalidId:
+        return jsonify({'error': 'Invalid ID format'}), 400
+    except Exception as e:
+        return jsonify({'error': 'An error occurred', 'message': str(e)}), 500
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
